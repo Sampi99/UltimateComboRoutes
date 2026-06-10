1b21241edaed3c87bcfff230b56950c12106a77c
Jorge Sampedro Calo <jorge.scalo@udc.es>
2026-06-10 13:31:58 +0200

    Primer commit

---
+++ src/test/java/tfc/project/ultimateComboRoutes/UltimateComboRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package tfc.project.ultimateComboRoutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateComboRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
