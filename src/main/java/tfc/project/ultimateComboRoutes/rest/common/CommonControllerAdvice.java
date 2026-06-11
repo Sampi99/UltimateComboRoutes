9507db756a103da9655ea8270cbbb2e40328d018
Jorge Sampedro Calo <jorge.scalo@udc.es>
2026-06-11 12:38:06 +0200

    Implementación de clases destinandas a la gestión de errores

---
+++ src/main/java/tfc/project/ultimateComboRoutes/rest/common/CommonControllerAdvice.java
@@ -0,0 +1,65 @@
+package tfc.project.ultimateComboRoutes.rest.common;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import tfc.project.ultimateComboRoutes.model.exceptions.DuplicateInstanceException;
+import tfc.project.ultimateComboRoutes.model.exceptions.InstanceNotFoundException;
+
+public class CommonControllerAdvice {
+
+	private static final String INSTANCE_NOT_FOUND_EXCEPTION_CODE = "El elemento introducido no existe";
+
+	private static final String DUPLICATE_INSTANCE_EXCEPTION_CODE = "El elemento ya está registrado";
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ErrorsDto handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+		List<FieldErrorDto> fieldErrors = exception.getBindingResult().getFieldErrors().stream()
+				.map(error -> new FieldErrorDto(error.getField(), error.getDefaultMessage()))
+				.collect(Collectors.toList());
+
+		return new ErrorsDto(fieldErrors);
+
+	}
+
+	@ExceptionHandler(InstanceNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseBody
+	public ErrorsDto handleInstanceNotFoundException(InstanceNotFoundException exception, Locale locale) {
+
+		String nameMessage = messageSource.getMessage(exception.getName(), null, exception.getName(), locale);
+		String errorMessage = messageSource.getMessage(INSTANCE_NOT_FOUND_EXCEPTION_CODE,
+				new Object[] { nameMessage, exception.getKey().toString() }, INSTANCE_NOT_FOUND_EXCEPTION_CODE, locale);
+
+		return new ErrorsDto(errorMessage);
+
+	}
+
+	@ExceptionHandler(DuplicateInstanceException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ErrorsDto handleDuplicateInstanceException(DuplicateInstanceException exception, Locale locale) {
+
+		String nameMessage = messageSource.getMessage(exception.getName(), null, exception.getName(), locale);
+		String errorMessage = messageSource.getMessage(DUPLICATE_INSTANCE_EXCEPTION_CODE,
+				new Object[] { nameMessage, exception.getKey().toString() }, DUPLICATE_INSTANCE_EXCEPTION_CODE, locale);
+
+		return new ErrorsDto(errorMessage);
+
+	}
+}
