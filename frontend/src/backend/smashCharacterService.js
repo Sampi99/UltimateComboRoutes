e67cfa255456c702e6af61b23cb193583bc411be
Jorge Sampedro Calo <jorge.scalo@udc.es>
2026-06-13 17:03:11 +0200

    Acceso al backend de SmashCharacterService

---
+++ frontend/src/backend/smashCharacterService.js
@@ -0,0 +1,64 @@
+import {
+    fetchConfig,
+    appFetch,
+}from "./appFetch";
+
+export const uploadSmashCharacter = (
+    name,
+    description,
+    weight,
+    gravity,
+    render,
+    onSuccess,
+    onErrors
+) => 
+    appFetch(
+        "/smashCharacters/uploadCharacter", fetchConfig("POST", {name, descrpition, 
+            weight, gravity, render}),  
+            onSuccess,
+            onErrors
+        );
+
+export const updateSmashCharacter = (
+    id,
+    name,
+    description,
+    weight,
+    gravity,
+    render,
+    onSuccess,
+    onErrors
+) =>
+    appFetch(
+        `/smashCharacters/${id}/updateCharacter`, fetchConfig("PUT", {name, description,
+            weight, gravity, render}),
+            onSuccess,
+            onErrors
+        );
+
+export const showSmashCharacterDetails = (
+    id,
+    onSuccess
+) =>
+    appFetch(
+        `/smashCharacters/${id}`, fetchConfig("GET"),
+        onSuccess
+    );
+
+export const showSmashCharacters = (
+    onSuccess
+) =>
+    appFetch(
+        "/smashCharacters", fetchConfig("GET"),
+        onSuccess
+    );
+
+export const filterByName = (
+    name,
+    onSuccess
+) => 
+    appFetch(
+        `smashCharacters/filterCharacters?name=${name}`, fetchConfig("GET"),
+        onSuccess
+    );
+    
