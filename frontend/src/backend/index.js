c792f2a0bc0fe6ac90e9f84e6bdb9dc25233728e
Jorge Sampedro Calo <jorge.scalo@udc.es>
2026-06-13 17:03:37 +0200

    Index modificado

--- frontend/src/backend/index.js
+++ frontend/src/backend/index.js
@@ -1,6 +1,7 @@
 import { init } from "./appFetch";
 import * as administratorService from "./administratorService";
+import * as smashCharacterService from "./smashCharacterService";
 
 export { default as NetworkError } from "./NetworkError";
 
-export default { init, administratorService};
+export default { init, administratorService, smashCharacterService};
